c32c7cb819fcba16601ab515037c6d212f295525
andreirk <sokolik_85@list.ru>
2014-04-25 12:45:17 +0700

    fix conflict of reacent  mearge

--- notes/views.py
+++ notes/views.py
@@ -24,11 +24,9 @@ def index(request, page_number=1):
     # return HttpResponse(output)
 
     latest_note_list = []
-<<<<<<< HEAD
+
     for note in Note.objects.filter(is_published=True):
-=======
-    for note in Note.objects.all():
->>>>>>> 8f8032d29f487ace3c84acd0e2454acb5d36e0d5
+
     	if note.author.username == auth.get_user(request).username:
     		latest_note_list.append(note)
 
@@ -143,7 +141,7 @@ def change_note(request,note_id):
 
 
 
-<<<<<<< HEAD
+
 def del_note(request,note_id):
 	args = {}
 	args.update(csrf(request))
@@ -156,5 +154,4 @@ def del_note(request,note_id):
 		return render_to_response('notes/del_note.html/', args)
 	else:
 		return render_to_response('notes/del_note.html', args)
-=======
->>>>>>> 8f8032d29f487ace3c84acd0e2454acb5d36e0d5
+
